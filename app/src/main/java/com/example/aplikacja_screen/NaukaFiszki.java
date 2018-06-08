24375209419ff1874660f68ceb3cfb3d3bbe8b21
PiotrGozdziewski <p.gozdziewski4@gmail.com>
2018-06-08 16:18:22 +0200

    fiszki, plansza

--- app/src/main/java/com/example/aplikacja_screen/NaukaFiszki.java
+++ app/src/main/java/com/example/aplikacja_screen/NaukaFiszki.java
@@ -3,6 +3,7 @@ package com.example.aplikacja_screen;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,7 @@ public class NaukaFiszki extends AppCompatActivity {
     Button fiszka;
     Boolean odwroc = false;
     ImageButton next;
+    ImageButton back;
     int i = 0;
 
     ArrayList<String> pl;
@@ -35,6 +37,7 @@ public class NaukaFiszki extends AppCompatActivity {
         en = new ArrayList<String>();
         fiszka = (Button) findViewById(R.id.button7);
         next = (ImageButton) findViewById(R.id.imageButton);
+        back = (ImageButton)findViewById(R.id.imageButton2);
 
         //pobranie ID aktualnego zestawu
         SharedPreferences p = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
@@ -49,14 +52,17 @@ public class NaukaFiszki extends AppCompatActivity {
         }
 
         fiszka.setText(pl.get(0)); //pierwszy element z arrayList
+        fiszka.setBackgroundColor(Color.parseColor("#f9f9b6"));
 
         fiszka.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (odwroc == true) {
+                    fiszka.setBackgroundColor(Color.parseColor("#f9f9b6"));
                     fiszka.setText(pl.get(i));
                     odwroc = false;
                 } else {
+                    fiszka.setBackgroundColor(Color.parseColor("#b5f99a"));
                     fiszka.setText(en.get(i));
                     odwroc = true;
                 }
@@ -67,14 +73,27 @@ public class NaukaFiszki extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //kolejna fiszka
+                fiszka.setBackgroundColor(Color.parseColor("#f9f9b6"));
                 if (i == pl.size() - 1) {
                     i = -1;
                 }
                 fiszka.setText(pl.get(i + 1));
                 odwroc = false;
                 i++;
+            }
+        });
 
-
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fiszka.setBackgroundColor(Color.parseColor("#f9f9b6"));
+                i--;
+                if(i==-1)
+                {
+                    i=pl.size()-1;
+                }
+                fiszka.setText(pl.get(i));
+                odwroc=false;
             }
         });
     }
--- app/src/main/java/com/example/aplikacja_screen/PlanszaKwadratowa.java
+++ app/src/main/java/com/example/aplikacja_screen/PlanszaKwadratowa.java
@@ -52,6 +52,8 @@ public class PlanszaKwadratowa extends AppCompatActivity {
     Random random_lista;
     //od wątków
     Handler handler = new Handler();
+    Handler handler1 = new Handler();
+    static int i=0;
 
 
     public void ustawWartosci() {
@@ -130,6 +132,15 @@ public class PlanszaKwadratowa extends AppCompatActivity {
         while (cursor.moveToNext()) {
             pl.add(cursor.getString(2));
             en.add(cursor.getString(3));
+        }
+
+        if(i<=4)
+        {
+            dalej.setText("Dalej");
+        }
+        if(i==5)
+        {
+            dalej.setText("Zakończ");
         }
 
         int ilosc = pl.size();
@@ -3278,8 +3289,19 @@ public class PlanszaKwadratowa extends AppCompatActivity {
         dalej.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(PlanszaKwadratowa.this, PlanszaKwadratowa.class));
-            }
+                if(i==5){
+                    startActivity(new Intent(PlanszaKwadratowa.this,WyborKategorii.class));
+                    handler1.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            i=0;
+                        }
+                    }, 300);
+                }
+                if(i<=4) {
+                    startActivity(new Intent(PlanszaKwadratowa.this, PlanszaKwadratowa.class));
+                }
+                i++; }
         });
     }
 

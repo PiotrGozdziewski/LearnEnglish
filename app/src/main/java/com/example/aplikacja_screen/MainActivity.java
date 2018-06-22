8d4240bed9af2da15c880f9427740d13a5d061cb
Martyna Markiewicz <martyna.markiewicz01@gmail.com>
2018-06-22 22:10:43 +0200

    Eksportowanie bazy danych

--- app/src/main/java/com/example/aplikacja_screen/MainActivity.java
+++ app/src/main/java/com/example/aplikacja_screen/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.aplikacja_screen;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,6 +17,15 @@ import com.example.Database.Database;
 import com.example.SHA256;
 import com.example.Uzytkownik;
 import com.example.m.aplikacja_screen.R;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.FileChannel;
 
 public class MainActivity extends AppCompatActivity {
     public static final String PATH = "/sdcard/Download/";
@@ -25,6 +37,29 @@ public class MainActivity extends AppCompatActivity {
     TextView zapomniane_hasło;
     Database db;
 
+    public void exportDatabse() {
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+
+            if (sd.canWrite()) {
+                String currentDBPath = "/app/src/main/assets/LearnEnglish.db";
+                String backupDBPath = "learnEnglish.db";
+                File currentDB = new File(data, currentDBPath);
+                File backupDB = new File(sd, backupDBPath);
+
+                if (currentDB.exists()) {
+                    FileChannel src = new FileInputStream(currentDB).getChannel();
+                    FileChannel dst = new FileOutputStream(backupDB).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +72,9 @@ public class MainActivity extends AppCompatActivity {
         haslo = (TextView) findViewById(R.id.editText7);
         zapomniane_hasło = (TextView) findViewById(R.id.textView5);
 
+        //od zgrania bazy na telefon
         exportDatabse();
+        //
         zaloguj.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
